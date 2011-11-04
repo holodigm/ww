7186fbaf540a5573ce7a81ac8073f768051a2795
Kym McInerney <kym.mcinerney@digitalglue.com.au>
2011-11-04 17:16:00 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whalewatchers::Application.config.secret_token = 'c0468a366aafe76de7a7045da6f7eed1aeda7153dbf59ca5ab002a99c195dde9cf3a46a08cb2a2636ac36a0485d5e58bb7a36bb18ca36a7cc3dc71b630ed752f'
